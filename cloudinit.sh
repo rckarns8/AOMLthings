d5969186150550b65e7818aeafa5c883613e31df
Rachael Storo <42095420+rckarns8@users.noreply.github.com>
2023-04-27 16:15:44 -0400

    Update cloudinit.sh

--- cloudinit.sh
+++ cloudinit.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-#SBATCH --job-name=INIT      # Job name 
-#SBATCH --output=INIT.log     # Standard output and error log     
+#SBATCH --job-name=TEST      # Job name 
+#SBATCH --output=TEST.log     # Standard output and error log     
 
 #Install Miniconda
 
 mkdir -p ~/miniconda3
+
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+
 rm -rf ~/miniconda3/miniconda.sh
+
 ~/miniconda3/bin/conda init bash
+
 ~/miniconda3/bin/conda init zsh
 
-conda activate
-  
+exec bash
+
 conda install mamba -n base -c conda-forge
  
 mamba create -n mg-qc -c bioconda fastqc multiqc fastp seqkit
